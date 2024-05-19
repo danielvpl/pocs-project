c4daaf2c038f1cc59fb44d5fa492bcb2ffe04c17
Daniel Viana <danielvpl@gmail.com>
2024-05-18 23:46:12 -0300

    Initializing repository

---
+++ ConsoleGoogleCloudStorage/Program.cs
@@ -0,0 +1,55 @@
+﻿using Google.Apis.Auth.OAuth2;
+using Google.Cloud.Storage.V1;
+using System.Text;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        var client = StorageClient.Create();
+
+        // Create a bucket with a globally unique name
+        var bucketName = Guid.NewGuid().ToString();
+        var bucket = client.CreateBucket("pocsgcp", bucketName);
+
+        // Upload some files
+        var content = Encoding.UTF8.GetBytes("hello, world");
+        var obj1 = client.UploadObject(bucketName, "file1.txt", "text/plain", new MemoryStream(content));
+        var obj2 = client.UploadObject(bucketName, "folder1/file2.txt", "text/plain", new MemoryStream(content));
+
+        // List objects in the bucket
+        foreach (var obj in client.ListObjects(bucketName, ""))
+        {
+            Console.WriteLine(obj.Name);
+        }
+
+        // Download a file
+        using (var stream = File.OpenWrite("file1.txt"))
+        {
+            client.DownloadObject(bucketName, "file1.txt", stream);
+        }
+
+
+        /*try
+        {
+            string bucketName = "seu-nome-de-bucket";
+            string sharedkeyFilePath = "caminho-para-seu-arquivo-de-credenciais.json";
+
+            var credential = GoogleCredential.FromJson(System.IO.File.ReadAllText(sharedkeyFilePath));
+            var storageClient = StorageClient.Create(credential);
+
+            string filetoUpload = "caminho-para-seu-arquivo.txt";
+            using (var fileStream = new FileStream(filetoUpload, FileMode.Open, FileAccess.Read, FileShare.Read))
+            {
+                storageClient.UploadObject(bucketName, "demo.txt", "text/plain", fileStream);
+            }
+
+            Console.WriteLine("Arquivo enviado com sucesso!");
+            Console.ReadLine();
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Erro: {ex.Message}");
+        }*/
+    }
+}
---
+++ ConsoleNfsConnector/Program.cs
@@ -0,0 +1,62 @@
+﻿using System.Net;
+
+IPAddress nfsServerIP = IPAddress.Parse("142.44.227.192");
+
+//var credentials = new NetworkCredential("<<user>>", "<<pass>>");
+
+string nfsRootPath = @"\nfs-storage/";
+
+var clientNfs = new NFSLibrary.NfsClient(NFSLibrary.NfsClient.NfsVersion.V4);
+
+// IP, UserId, GroupId, Timeout
+// ISSUE: Where can I put the user credentials!?
+clientNfs.Connect(nfsServerIP, 267512, 287678, 10000);
+
+var directoryName = clientNfs.GetDirectoryName(nfsRootPath);
+
+var devices = clientNfs.GetExportedDevices();
+
+clientNfs.MountDevice(devices[0]);
+
+var files = clientNfs.GetItemList(directoryName);
+
+foreach (var file in files)
+{
+    Console.WriteLine("Reading file: " + file);
+    Console.WriteLine();
+
+    var localPath = Directory.GetCurrentDirectory() + "\\downloads\\";
+    
+    if(!Directory.Exists(localPath))
+        Directory.CreateDirectory(localPath);
+    
+    clientNfs.Read($"{directoryName}\\{file}", localPath + file);
+    
+    var fileBytes = File.ReadAllBytes(localPath + file);
+    
+    Console.WriteLine("File bytes length: " + fileBytes.Length);
+    Console.WriteLine();
+
+    StreamReader sr = new StreamReader(localPath + file);
+
+    // Read the first line of text
+    var line = sr.ReadLine();    
+    Console.WriteLine("File Content: ");
+    Console.WriteLine();
+    // Continue reading until the end of the file
+    while (line != null)
+    {
+        // Write the line to the console window
+        Console.WriteLine(line);
+
+        // Read the next line
+        line = sr.ReadLine();
+    }
+
+    // Close the file
+    sr.Close();    
+}
+
+Console.WriteLine();
+Console.WriteLine("Processo de obtenção e leitura de arquivo finalizado.");
+Console.ReadKey();
